d6285be0d08a00750aa42788e7561317ecf64384
20-Royston <coldroy1235@gmail.com>
2022-10-06 11:58:29 +0800

    added in flare script

---
+++ Assets/flarescript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flarescript : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
